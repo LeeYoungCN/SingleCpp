bb48658371a448d7beebd38bea1fd5b79c4a3005
LiYang <1175529741@qq.com>
2022-04-12 19:59:21 +0800

    加宏

--- half_byte.c
+++ half_byte.c
@@ -3,12 +3,13 @@
 
 #define ONE_STEP_MV_BIT_NUM 4
 #define BIT_NUM_IN_ONE_BYTE 8
+#define BIT_NUM(X) sizeof(X) * BIT_NUM_IN_ONE_BYTE
 
 VOS_INT32 main(VOS_INT32 argc, VOS_INT8 **argv)
 {
     VOS_UINT32 num_arr[] = {1, 2, 3, 4, 5};
     VOS_UINT32 ret = 0;
-    VOS_UINT32 maxMvBitNum = sizeof(ret) * BIT_NUM_IN_ONE_BYTE;
+    VOS_UINT32 maxMvBitNum = BIT_NUM(ret);
     for (VOS_UINT32 i = 0; i < ITEM_OF(num_arr) ; i++) {
         VOS_UINT32 mvBitNum = ONE_STEP_MV_BIT_NUM * i;
         if (mvBitNum > maxMvBitNum) {
