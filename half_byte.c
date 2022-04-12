fae4d30c47b94bdfe03fde7b31c36b202068deeb
LiYang <1175529741@qq.com>
2022-04-12 19:55:37 +0800

    c

---
+++ half_byte.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "public_def.h"
+
+#define ONE_STEP_MV_BIT_NUM 4
+#define BIT_NUM_IN_ONE_BYTE 8
+
+VOS_INT32 main(VOS_INT32 argc, VOS_INT8 **argv)
+{
+    VOS_UINT32 num_arr[] = {1, 2, 3, 4, 5};
+    VOS_UINT32 ret = 0;
+    VOS_UINT32 maxMvBitNum = sizeof(ret) * BIT_NUM_IN_ONE_BYTE;
+    for (VOS_UINT32 i = 0; i < ITEM_OF(num_arr) ; i++) {
+        VOS_UINT32 mvBitNum = ONE_STEP_MV_BIT_NUM * i;
+        if (mvBitNum > maxMvBitNum) {
+            break;
+        }
+        ret |= (num_arr[i] << mvBitNum);
+    }
+    printf("ret = %#lx\n", ret);
+    return 0;
+}
--- public_def.h
+++ public_def.h
@@ -1,9 +1,11 @@
 #ifndef PUBLIC_DEF_H
 #define PUBLIC_DEF_H
 
-using VOS_UINT32    = unsigned int;
-using VOS_INT32     = int;
-using VOS_UINT8     = unsigned char;
-using VOS_INT8      = char;
+#define VOS_UINT32 unsigned int
+#define VOS_INT32           int
+#define VOS_UINT8  unsigned char
+#define VOS_INT8            char
+
+#define ITEM_OF(array) (VOS_UINT32)(sizeof(array)/sizeof(array[0]))
 
 #endif // PUBLIC_DEF_H
